b4dce1e07e9e741e0642e06e27bacd34b76d0821
rickymoca <ricky.mocaa@gmail.com>
2020-03-18 22:05:43 +0700

    oke

--- application/modules/campaign/views/v_campaign.php
+++ application/modules/campaign/views/v_campaign.php
@@ -2,16 +2,16 @@
     <div class="card">
         <div class="card-header header-elements-inline">
 
-            <h3 class="card-title"> <i class="mi-record-voice-over mr-3 mi-2x"></i>INFO TRIAL PRODUCT</h3>
-            <a href="" class="text-danger" data-toggle="modal" data-target="#modal_theme_primary">
-                <i class="mi-arrow-drop-down-circle mr-3 mi-2x"></i>
+            <h3 class="card-title"> <i class="icon-stats-growth mr-3 icon-2x"></i>INFO TRIAL PRODUCT</h3>
+            <a href="" class="text-blue" data-toggle="modal" data-target="#modalCampaign">
+                <i class="icon-folder-plus2 mr-3 icon-2x" data-popup="tooltip" data-original-title="Add New Campaign"></i>
             </a>
 
         </div>
 
 
         <div class="card-body">
-            <table class="table datatable-basic">
+            <table class="table datatable-basic text-dark">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -35,7 +35,7 @@
                             <td>Coredash Beli 2 gratis 1</td>
                             <td>145,000</td>
                             <td>B1G1 pembelian 1 pcs</td>
-                            <td>Http://supernat.id/page2</td>
+                            <td> <a href="Http://supernat.id/page2">Http://supernat.id/page2</a></td>
 
                             <td>
                                 <div class="form-check form-check-switchery">
@@ -50,10 +50,11 @@
                             </td>
                             <td>
 
-                                <button type="button" class="btn btn-sm bg-light" data-popup="tooltip" data-original-title="View More">
+                                <a href="campaign/detail" type="button" class="btn btn-sm bg-light" data-popup="tooltip" data-original-title="View More">
                                     <i class="mi-visibility"></i>
-                                </button>
-                                <button type="button" class="btn btn-sm bg-light" data-popup="tooltip" data-original-title="Go to Link page">
+                                </a>
+
+                                <button type="button" class="btn btn-sm bg-light" data-popup="tooltip" data-original-title="Result">
                                     <i class="mi-near-me"></i>
                                 </button>
 
@@ -70,65 +71,82 @@
 
 
 <!-- Primary modal  Add User-->
-<div id="modal_theme_primary" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
+<div id="modalCampaign" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header bg-slate-600">
-                <h6 class="modal-title">Primary header</h6>
+            <div class="modal-header bg-dark">
+
+                <h6 class="modal-title"><i class="icon-file-plus2 mr-1 icon-2x"></i> Creat new campaign trial</h6>
                 <button type="button" class="close" data-dismiss="modal">×</button>
             </div>
 
             <div class="modal-body">
-                <form class="user" method="post" action="<?= base_url('admin/registration'); ?>">
+
+                <form class="user" method="post" action="<?= base_url('todo/addtodo'); ?>">
                     <div class="form-group">
-                        <input type="text" class="form-control" disabled="true" id="iduser" name="iduser" value="<?= $id ?>">
+                        <small class="mb-1 badge badge-light">Campaign Name</small>
+                        <input type="text" class="form-control" id="subject" name="subject" value="<?= set_value('subject'); ?>">
+                        <!-- form validation -->
+                        <?php if (form_error('subject')) : ?>
+                            <small class="text-danger"><?= form_error('subject') ?></small>
+                        <?php else : ?> <?php endif; ?>
+                        <!-- end Form validation -->
                     </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" value="<?= set_value('name'); ?>">
-                        <?= form_error('name', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email" value="<?= set_value('email'); ?>">
-                        <?= form_error('email', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
+
+
 
                     <div class="form-row mb-4">
                         <div class="col-6">
-                            <input type="password" class="form-control" id="password1" name="password1" placeholder="Password">
-                            <?= form_error('password1', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
-                        <div class="col">
-                            <input type="password" class="form-control" id="password2" name="password2" placeholder="Confirm Password">
-                            <?= form_error('password2', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
+                            <small class="mb-1 badge badge-light">Select Product Name</small>
 
-                    </div>
-                    <div class="form-row mb-4">
-                        <div class="col-2">
-                            &nbsp;<label> Status :</label>
-                        </div>
-                        <div class="col">
-                            <label class="switch size-sm">
-                                <input type="checkbox" checked="true" value="1" name="status">
-                                <span class="slider round success"></span>
-                            </label>
-                        </div>
-                        <div class="col">
-                            <select id="inputState" name="hakakses" class="form-control">
-                                <option selected="true">-Select Hakases-</option>
-                                <?php foreach ($getrole as $m) : ?>
-                                    <option value="<?= $m['id']; ?>"><?= $m['role']; ?></option>
+                            <select id="id_user" name="user_recived" class="form-control">
+                                <?php foreach ($Vuser as $u) : ?>
+                                    <option value="<?= $u['id']; ?>"><?= $u['name']; ?><i class="icon-user"></i></option>
                                 <?php endforeach; ?>
                             </select>
+                            <!-- form validation -->
+                            <?php if (form_error('user_recived')) : ?>
+                                <small class="text-danger"><?= form_error('user_recived') ?></small>
+                            <?php else : ?> <?php endif; ?>
+                            <!-- end Form validation -->
+                        </div>
+
+                        <div class="col-6">
+                            <small class="mb-1 badge badge-light">Discount Price</small>
+
+                            <div class="input-group">
+                                <span class="input-group-prepend">
+                                    <span class="input-group-text"><i class="icon-percent"></i></span>
+                                </span>
+                                <input type="text" name="duedate" class="form-control daterange-single" value="<?= set_value('duedate'); ?>">
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <span class="input-group-prepend">
+                                <span class="input-group-text"><i class="icon-link"></i></span>
+                            </span>
+                            <input type="text" name="duedate" class="form-control" placeholder="Enter Landing Page Link" value="<?= set_value('duedate'); ?>">
                         </div>
                     </div>
 
+                    <div class="form-group">
+
+                        <textarea type="text-area" class="form-control" id="message" name="message" placeholder="Enter your notes for campaign" value=""></textarea>
+                        <!-- form validation -->
+                        <?php if (form_error('message')) : ?>
+                            <small class=" text-danger"><?= form_error('message') ?></small>
+                        <?php else : ?> <?php endif; ?>
+                        <!-- end Form validation -->
+                    </div>
 
             </div>
 
             <div class="modal-footer">
                 <button type="button" class="btn bg-primary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn bg-slate-600">Save changes</button>
+                <button type="submit" id="BtnaddTodo" class="btn bg-slate-600">Save changes</button>
             </div>
             </form>
         </div>
@@ -138,71 +156,13 @@
 
 
 
-<!-- Primary modal  Edit User-->
 
-<div id="modal_edit" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header bg-slate-600">
-                <h6 class="modal-title">Edit user</h6>
-                <button type="button" class="close" data-dismiss="modal">×</button>
-            </div>
-
-            <div class="modal-body">
-                <form class="user" method="post" action="<?= base_url('admin/registration'); ?>">
-                    <div class="form-group">
-                        <input type="text" class="form-control" disabled="true" id="iduser" name="iduser" value="">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" value="<?= set_value('name'); ?>">
-                        <?= form_error('name', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email" value="<?= set_value('email'); ?>">
-                        <?= form_error('email', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
-
-                    <div class="form-row mb-4">
-                        <div class="col-6">
-                            <input type="password" class="form-control" id="password1" name="password1" placeholder="Password">
-                            <?= form_error('password1', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
-                        <div class="col">
-                            <input type="password" class="form-control" id="password2" name="password2" placeholder="Confirm Password">
-                            <?= form_error('password2', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
-
-                    </div>
-                    <div class="form-row mb-4">
-                        <div class="col-2">
-                            &nbsp;<label> Status :</label>
-                        </div>
-                        <div class="col">
-                            <label class="switch size-sm">
-                                <input type="checkbox" checked="true" value="1" name="status">
-                                <span class="slider round success"></span>
-                            </label>
-                        </div>
-                        <div class="col">
-                            <select id="inputState" name="hakakses" class="form-control">
-                                <option selected="true">-Select Hakases-</option>
-                                <?php foreach ($getrole as $m) : ?>
-                                    <option value="<?= $m['id']; ?>"><?= $m['role']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-                    </div>
-
-
-            </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn bg-primary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn bg-slate-600">Save changes</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
-<!-- End Primary modal -->
 <script src="<?= base_url(); ?>assets/vendor/js/demo_pages/form_checkboxes_radios.js"></script>
+<script>
+    $(document).ready(function() {
+        var modal = "<?= $this->session->flashdata('modal'); ?>"
+        if (modal == "eror") {
+            $("#modalCampaign").modal('show');
+        }
+    });
+</script>
--- application/modules/campaign/views/v_campaign_detail.php
+++ application/modules/campaign/views/v_campaign_detail.php
@@ -1,62 +1,82 @@
 <div class="col-lg-12">
     <div class="card">
-        <div class="card-header header-elements-inline">
-            <h1 class="card-title"> <i class="icon-info22 mr-2 icon-2x"></i>Detail Promo</h1>
+
+        <div class="col-lg-8 mx-auto mb-2 mt-5">
+
+            <div class="col-md-auto  bg-light px-3 py-3 mx-auto mt-1">
+                <h4 class="card-title"> <i class="icon-info22 mr-2 icon-1x"></i>Detail Promo</h4>
+
+                <div class="float-right">
+                    <input type="checkbox" class="form-check-input-switchery" name="status" checked data-fouc>
+                </div>
+                <h1><strong>Promo Coredash</strong></h1>
+                <hr>
+                <h3><i class="icon-cash3 mr-2 icon-1x"></i><strong>Rp.175,000 </strong></h3>
+                <h6><i class="icon-unlink mr-2 icon-1x"></i><strong> <a href=""> http://gudangperkakas.com/lp-1</a> </strong></h6>
+                <h6><i class="icon-clipboard6 mr-2 icon-1x"></i>Beli 1 Gratis 1 Free ongkir</h6>
+            </div>
+
         </div>
 
-        <div class="card-body">
+        <!-- Coment Divender -->
+        <div class="col-lg-8 mx-auto mt-4">
             <div class="row">
-                <div class="col-md-auto  bg-light border-right-3 border-right-grey px-3 py-3 mt-1">
-                    <h1><strong> Coredash</strong></h1>
-                    <hr>
-                    <h3><i class="icon-cash3 mr-2 icon-2x"></i><strong>Rp.175,000 </strong></h3>
-                    <h6><i class="icon-unlink mr-2 icon-2x"></i><strong> </strong>: <a href=""> http://gudangperkakas.com/lp-1</a></h6>
-                    <h6><i class="icon-clipboard6 mr-2 icon-2x"></i> <strong>Note </strong>: Beli 1 Gratis 1 Free ongkir</h6>
-                    <h5><strong>Status </strong>: Active</h5>
+                <div class="col-lg-3">
+                    <h1> <strong> COMMENTS</strong>
+                        <span class="badge badge-warning">1</span>
+                    </h1>
                 </div>
+                <div class="col">
+                    <hr class="border-1">
+                </div>
+
             </div>
         </div>
-    </div>
+        <!-- End Coment Dive -->
+
+        <div class="col-lg-8 mx-auto mb-5">
+            <div class="card bg-light">
+
+                <ul class="mt-3">
+
+                    <li class="media">
+                        <div class="text-grey"> <i class="mi-account-circle mi-3x mr-2"></i></div>
+
+                        <div class="media-chat-item col-md-10">
+                            <div class="font-size-sm text-muted mt-2 float-right"> Mart, 17 </div>
+                            <strong>Mohamad Ricky</strong>
+                            <h6> Itu Maksudnya gmna kang?</h6>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <div class="text-grey"> <i class="mi-account-circle mi-3x mr-2"></i></div>
+
+                        <div class="media-chat-item col-md-10">
+                            <div class="font-size-sm text-muted mt-2 float-right"> Mart, 18 </div>
+                            <strong>Sarah NHK</strong>
+                            <h6> iya gitu we pokonya mah wkwkwk</h6>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </div>
 
 
-</div>
+            <div class="row text-center">
+                <div class="col-md-10">
+                    <input type="text" class="form-control  my-3 bg-light border-1 border-grey font-weight-semibold" placeholder="Enter your comment...">
+                </div>
+                <div class="col-md-2">
+                    <button type="button" class="btn bg-dark my-3 btn-labeled btn-labeled-right"><b><i class="icon-paperplane"></i></b> Send</button>
+                </div>
+            </div>
 
-<div class="col-lg-12 mx-auto">
-    <div class="card bg-light">
-        <div class="card-title px-3 py-1 bg-grey">
-            <h1 class="text-light"> <i class="mi-textsms mr-1 mi-3x"></i> <strong>COMMENT</strong></h1>
+
         </div>
 
-        <div class="card-body bg-light">
 
-            <ul class="mt-5">
-                <li class="media">
-                    <div class="mr-3 text-grey">
-                        <i class="mi-account-circle mi-3x"></i>
-                    </div>
-
-                    <div class="media-body">
-                        <div class="media-chat-item">Itu maksudnya beli2 gratis 1 pcs coredas gitu ya ?</div>
-                        <div class="font-size-sm text-muted mt-2">Mohamad Ricky Mon, 9:54 am </div>
-                    </div>
-                </li>
-
-                <li class="media media-chat-item-reverse">
-                    <div class="media-body">
-                        <div class="media-chat-item">Iyups Betul bray</div>
-                        <div class="font-size-sm text-muted mt-2">Me Mon, 9:54 am </div>
-                    </div>
-
-                    <div class="ml-2 text-blue">
-                        <i class="mi-account-circle mi-3x"></i>
-                    </div>
-                </li>
-
-                <textarea name="enter-message" class="form-control my-3 bg-light" rows="3" cols="1" placeholder="Enter your comment..."></textarea>
-                <button type="button" class="btn bg-dark btn-labeled btn-labeled-right float-right"><b><i class="icon-paperplane"></i></b> Send</button>
-        </div>
     </div>
-</div>
 </div>
 
 
@@ -128,72 +148,4 @@
 <!-- End Primary modal -->
 
 
-
-<!-- Primary modal  Edit User-->
-
-<div id="modal_edit" class="modal fade" tabindex="-1" aria-hidden="true" style="display: none;">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header bg-slate-600">
-                <h6 class="modal-title">Edit user</h6>
-                <button type="button" class="close" data-dismiss="modal">×</button>
-            </div>
-
-            <div class="modal-body">
-                <form class="user" method="post" action="<?= base_url('admin/registration'); ?>">
-                    <div class="form-group">
-                        <input type="text" class="form-control" disabled="true" id="iduser" name="iduser" value="">
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="name" name="name" placeholder="Enter name" value="<?= set_value('name'); ?>">
-                        <?= form_error('name', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
-                    <div class="form-group">
-                        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email" value="<?= set_value('email'); ?>">
-                        <?= form_error('email', '<small class="text-danger pl-1">', '</small>'); ?>
-                    </div>
-
-                    <div class="form-row mb-4">
-                        <div class="col-6">
-                            <input type="password" class="form-control" id="password1" name="password1" placeholder="Password">
-                            <?= form_error('password1', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
-                        <div class="col">
-                            <input type="password" class="form-control" id="password2" name="password2" placeholder="Confirm Password">
-                            <?= form_error('password2', '<small class="text-danger pl-1">', '</small>'); ?>
-                        </div>
-
-                    </div>
-                    <div class="form-row mb-4">
-                        <div class="col-2">
-                            &nbsp;<label> Status :</label>
-                        </div>
-                        <div class="col">
-                            <label class="switch size-sm">
-                                <input type="checkbox" checked="true" value="1" name="status">
-                                <span class="slider round success"></span>
-                            </label>
-                        </div>
-                        <div class="col">
-                            <select id="inputState" name="hakakses" class="form-control">
-                                <option selected="true">-Select Hakases-</option>
-                                <?php foreach ($getrole as $m) : ?>
-                                    <option value="<?= $m['id']; ?>"><?= $m['role']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        </div>
-                    </div>
-
-
-            </div>
-
-            <div class="modal-footer">
-                <button type="button" class="btn bg-primary" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn bg-slate-600">Save changes</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
-<!-- End Primary modal -->
 <script src="<?= base_url(); ?>assets/vendor/js/demo_pages/form_checkboxes_radios.js"></script>
